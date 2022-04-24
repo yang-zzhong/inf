51be0d5556bccba4cfaac896dfcbc0c0daea8607
Oliver Yang <oliverzyang@tencent.com>
2022-04-24 18:34:44 +0800

    xxx

--- btree_test.go
+++ btree_test.go
@@ -5,47 +5,34 @@ import (
 	"testing"
 )
 
-func printv(n *node, key string, t *testing.T) {
-	val, err := n.root().get([]byte("hello"))
+func printv(tree *btree, key string, t *testing.T) {
+	val, err := tree.Get(Key([]byte("hello")))
 	if err != nil {
 		t.Fatalf("%s", err.Error())
 	}
-	fmt.Printf("%s\n", val)
+	fmt.Printf("%s\n", val.(pair).Val)
 }
 
 func TestNode_put(t *testing.T) {
-	n := &node{total: 16}
-	n.put([]byte("00"), []byte("00"))
-	n = n.root()
-	n.put([]byte("02"), []byte("02"))
-	n = n.root()
-	n.put([]byte("04"), []byte("04"))
-	n = n.root()
-	n.put([]byte("06"), []byte("06"))
-	n = n.root()
-	n.put([]byte("08"), []byte("08"))
-	n = n.root()
-	n.put([]byte("10"), []byte("10"))
-	n = n.root()
-	n.put([]byte("01"), []byte("01"))
-	n = n.root()
-	n.put([]byte("03"), []byte("03"))
-	n = n.root()
-	n.put([]byte("05"), []byte("05"))
-	n = n.root()
-	n.put([]byte("07"), []byte("07"))
-	n = n.root()
-	n.put([]byte("09"), []byte("09"))
-	n = n.root()
+	tree := NewTree(16)
+	tree.Put(Pair([]byte("00"), []byte("00")))
+	tree.Put(Pair([]byte("02"), []byte("02")))
+	tree.Put(Pair([]byte("04"), []byte("04")))
+	tree.Put(Pair([]byte("06"), []byte("06")))
+	tree.Put(Pair([]byte("08"), []byte("08")))
+	tree.Put(Pair([]byte("10"), []byte("10")))
+	tree.Put(Pair([]byte("01"), []byte("01")))
+	tree.Put(Pair([]byte("03"), []byte("03")))
+	tree.Put(Pair([]byte("05"), []byte("05")))
+	tree.Put(Pair([]byte("07"), []byte("07")))
+	tree.Put(Pair([]byte("09"), []byte("09")))
 }
 
 func TestNode_del(t *testing.T) {
-	n := &node{total: 16}
-	for i := 0; i <= 10; i++ {
+	tree := NewTree(16)
+	for i := 0; i <= 6; i++ {
 		k := []byte(fmt.Sprintf("%02d", i))
-		n.put(k, k)
-		n = n.root()
+		tree.Put(Pair(k, k))
 	}
-	n.del([]byte("00"))
-	n = n.root()
+	tree.Del(Key([]byte("00")))
 }
